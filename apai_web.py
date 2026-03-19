d2d871ffce0d62ad26fe1db92e8d281203ab5300
Adnan Sadik <adnansadik235@gmail.com>
2026-03-19 09:50:26 +0800

    changed binding url

--- apai_web.py
+++ apai_web.py
@@ -21,5 +21,8 @@ def ask():
         "history": history
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
